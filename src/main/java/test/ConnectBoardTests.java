b5a1a8ddf69b6cedfbf118c07ddc8d234db618e3
Alan Rosenberg <alanrosenberg77@gmail.com>
2023-10-12 10:27:18 -0500

    tested addDisk
    tested normal use case and use case with full column

--- src/main/java/test/ConnectBoardTests.java
+++ src/main/java/test/ConnectBoardTests.java
@@ -11,41 +11,96 @@ class ConnectBoardTests {
 	
 	ConnectBoard blankBoard;
 	ConnectBoard sampleBoard;
+	int[][] testBoard = {
+			{0,0,0,0,0,0,0,0,0,0},
+			{1,-1,1,1,-1,1,-1,-1,-1,1},
+			{1,0,0,0,0,0,0,0,0,0},
+			{-1,0,0,0,0,0,0,0,0,0},
+			{-1,-1,1,0,0,0,0,0,0,0},
+			{1,1,-1,0,0,0,0,0,0,0},
+			{-1,1,1,-1,0,0,0,0,0,0},
+			{0,0,0,0,0,0,0,0,0,0},
+			{1,-1,0,0,0,0,0,0,0,0},
+			{-1,1,1,1,1,0,0,0,0,0}};
 
 	@BeforeEach
 	void setUp() throws Exception {
 		
 		blankBoard = new ConnectBoard(10, 10);
 		sampleBoard = new ConnectBoard(10, 10);
-		int[][] testBoard = {
-				{0,0,0,0,0,0,0,0,0,0},
-				{1,-1,1,1,-1,1,-1,-1,-1,1},
-				{1,0,0,0,0,0,0,0,0,0},
-				{-1,0,0,0,0,0,0,0,0,0},
-				{-1,-1,1,0,0,0,0,0,0,0},
-				{1,1,-1,0,0,0,0,0,0,0},
-				{-1,1,1,-1,0,0,0,0,0,0},
-				{0,0,0,0,0,0,0,0,0,0},
-				{1,-1,0,0,0,0,0,0,0,0},
-				{-1,1,1,1,1,0,0,0,0,0}};
 		
 		sampleBoard.setBoard(testBoard);
 		
 	}
+	
+	/*
+	 * Testing isColumnFull
+	 */
 
+	/*
+	 * Testing that isColumnFull works properly with an empty column
+	 */
 	@Test
 	void test_isColumnFull_EmptyColumn() {
 		assertFalse(blankBoard.isColumnFull(0));
 	}
 	
+	/*
+	 * Testing that isColumnFull works properly with a non-empty, non-full column
+	 */
 	@Test
 	void test_isColumnFull_NonFullColumn() {
 		assertFalse(sampleBoard.isColumnFull(4));
 	}
 	
+	/*
+	 * Testing that isColumnFull works properly with a full column
+	 */
 	@Test
 	void test_isColumnFull_FullColumn() {
 		assertTrue(sampleBoard.isColumnFull(1));
+	}
+	
+	/*
+	 * Testing addDisk
+	 */
+	
+	/*
+	 * Testing that addDisk works properly with non-full columns
+	 */
+	@Test
+	void test_addDisk_Normal() {
+		
+		int row1 = blankBoard.addDisk(0, blankBoard.USER);
+		int row2 = sampleBoard.addDisk(4, sampleBoard.AGENT);
+		
+		assertEquals(0, row1);
+		assertEquals(3, row2);
+		
+		int[][] blank = blankBoard.getBoard();
+		int[][] sample = sampleBoard.getBoard();
+		
+		assertEquals(1, blank[0][0]);
+		assertEquals(-1, sample[4][3]);
+	}
+	
+	/*
+	 * Testing that addDisk works properly with a full column
+	 */
+	@Test
+	void test_addDisk_FullColumn() {
+		
+		int row = sampleBoard.addDisk(1, sampleBoard.USER);
+		
+		assertEquals(-1, row);
+		
+		int[][] sample = sampleBoard.getBoard();
+		
+		for(int i = 0 ; i < sample[0].length ; i++) {
+			
+			assertEquals(testBoard[0][i], sample[0][i]);
+			
+		}
 	}
 
 }
