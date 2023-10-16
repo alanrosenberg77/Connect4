1d3df856f64dbb19325c2f660f491fd0270c734d
alanrosenberg77 <alanrosenberg77@gmail.com>
2023-10-16 16:43:05 -0500

    tests fixed
    finally

--- src/test/java/cs440/c4/ConnectBoardTests.java
+++ src/test/java/cs440/c4/ConnectBoardTests.java
@@ -1,11 +1,13 @@
-package test;
+package cs440.c4;
 
-import static org.junit.jupiter.api.Assertions.*;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import cs440.c4.ConnectBoard;
 
 class ConnectBoardTests {
 	
